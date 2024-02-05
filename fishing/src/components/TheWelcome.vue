5fe4514611cd2c0666dbcfbd7975cfa822092604
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-02-05 12:33:52 -0500

    unfished

--- fishing/src/components/TheWelcome.vue
+++ fishing/src/components/TheWelcome.vue
@@ -1,30 +1,36 @@
 <template>
-  <div>
-    <h1 v-if="bool">hooray</h1>
-    <h1 v-else>booray</h1>
-    <ul>
-      <li v-for="animal in animals">holy hell, is that {{ animal }}</li>
-    </ul>
-  </div>
+    <div>
+        <h1 v-if="bool">hooray</h1>
+        <h1 v-else>booray</h1>
+        <ul>
+            <li v-for="(character, index) in character" :key="character.name">
+                holy hell, is that {{ character.name }} with net worth of ${{ character.money }} index number {{ index }}
+            </li>
+        </ul>
+        <button @click="scream">press me</button>
+    </div>
 </template>
 
 <script setup>
-const bool = true
-const animals = ["joe", "not joe", "evil joe"]
+const bool = true;
+function scream() {
+    alert("STOPPP");
+}
+const props = defineProps(["character"]);
 </script>
 
 <style lang="css" scoped>
 h1 {
-  text-decoration: underline;
-  text-transform: uppercase;
+    text-decoration: underline;
+    text-transform: uppercase;
 }
 
 li::before {
-  content: "💀";
-  margin-right: 1ch;
+    content: "💀";
+    margin-right: 1ch;
 }
 
 li {
-  text-shadow: 0 0 5px red;
+    text-shadow: 0 0 5px red;
 }
 </style>
--- fishing/src/views/HomeView.vue
+++ fishing/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+const joe = [
+    {
+        name: "mario",
+        money: 500,
+    },
+    {
+        name: "luigi",
+        money: 5000,
+    },
+    {
+        name: "wario",
+        money: -500000,
+    },
+    {
+        name: "waluigi",
+        money: 0,
+    },
+];
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <TheWelcome v-for="character in joe" :key="character.name" :item="character" />
+    </main>
 </template>
