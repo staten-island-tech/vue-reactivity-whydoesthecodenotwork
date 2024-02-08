5fb980c2df9b42f0fc0e4ac213e0e10604c4aec1
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-02-08 12:32:32 -0500

    so many colons

---
+++ fishing/src/components/FisherCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    fisher: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div>
+        <p>it's {{ fisher.name }}</p>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ fishing/src/components/Fishers.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    fishers: {
+        type: Array,
+        required: true,
+    },
+});
+import FisherCard from "./FisherCard.vue";
+</script>
+
+<template>
+    <div>
+        <h2>fishers</h2>
+        <div id="fishers">
+            <FisherCard v-for="fisher in props.fishers" :key="fisher.name" :fisher="fisher"></FisherCard>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
