ac50a346d85666dd4ce1f6ffe33a5fce62e16d97
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-02-12 13:24:28 -0500

    bwuh??

--- timer/src/components/Timer.vue
+++ timer/src/components/Timer.vue
@@ -1,24 +1,31 @@
 <template>
     <div>
-        <h2>{{ Timer.name }}</h2>
+        <input type="text" v-model="name" />
         <p>{{ Timer.left }} / {{ Timer.length }}</p>
+        <meter min="0" :max="Timer.length" :value="Timer.left"></meter>
     </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, defineModel } from "vue";
 
 const props = defineProps({
     Timer: "Object",
 });
 
+const emits = defineEmits(["explode"]);
+
+const name = defineModel();
+
 onMounted(() => {
     const timer = props.Timer;
+    name.value = timer.name;
     timer.left = timer.length;
     const joe = setInterval(() => {
         timer.left--;
-        console.log("IAIWNEOWNJO");
         if (timer.left < 1) {
+            console.log("oh BROTHER. this just exploded: " + name.value);
+            emits("explode", timer);
             clearInterval(joe);
         }
     }, 1000);
--- timer/src/views/Main.vue
+++ timer/src/views/Main.vue
@@ -6,15 +6,21 @@ import { ref } from "vue";
 
 const timers = useTimers();
 const { timerList, unhelpfulLength } = storeToRefs(timers);
+
+function explode(Timer) {
+    console.log("wow");
+    timerList.value.splice(timerList.value.indexOf(Timer), 1);
+}
 </script>
 
 <template>
     <main>
         <div id="timers" v-if="unhelpfulLength > 0">
             <h1>you have (0.5*{{ unhelpfulLength }}) timers:</h1>
-            <Timer v-for="Timer in timerList" :key="Timer.name" :Timer="Timer" />
+            <!-- student uses ENTIRE OBJECT as key, asked to leave APCSP -->
+            <Timer v-for="Timer in timerList" :key="Timer" :Timer="Timer" @explode="explode" />
         </div>
         <h1 v-else>you have NO timers...</h1>
-        <button @click="timers.addTimer('this is name', 60)">oh man</button>
+        <button @click="timers.addTimer('this is name', 5)">oh man</button>
     </main>
 </template>
